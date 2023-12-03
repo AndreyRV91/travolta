5a8891a2ca7d0e6aacdc6b6c31c1f6a016ea5937
Andrey Rybakov <andreyrv91@gmail.com>
2023-12-03 22:44:22 +0600

    add tsconfig for cypress. Fix data-test-id names

--- src/components/SearchForm.cy.ts
+++ src/components/SearchForm.cy.ts
@@ -10,18 +10,18 @@ describe('<SearchForm />', () => {
 
     cy.get('form').submit()
 
-    cy.get('[data-testid=search-destination]').should('contain', 'Please input destination')
-    cy.get('[data-testid=search-checkin]').should('contain', 'Please pick a date')
-    cy.get('[data-testid=search-checkout]').should('contain', 'Please pick a date')
+    cy.get('[data-test-id=search-destination]').should('contain', 'Please input destination')
+    cy.get('[data-test-id=search-checkin]').should('contain', 'Please pick a date')
+    cy.get('[data-test-id=search-checkout]').should('contain', 'Please pick a date')
   })
 
   it('submits form and redirects to /hotels', () => {
     cy.mount(SearchForm)
 
-    cy.get('[data-testid=search-destination]').type('Boston')
-    cy.get('[data-testid=search-checkin]').type('2223-12-01')
-    cy.get('[data-testid=search-checkout]').type('2223-12-15')
-    cy.get('[data-testid=submit-button]')
+    cy.get('[data-test-id=search-destination]').type('Boston')
+    cy.get('[data-test-id=search-checkin]').type('2223-12-01')
+    cy.get('[data-test-id=search-checkout]').type('2223-12-15')
+    cy.get('[data-test-id=submit-button]')
       .click()
       .then(() => {
         cy.url().should('include', '/hotels')
